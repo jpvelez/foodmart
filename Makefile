4c6d3472f1b7ac8603b79cc2f0feaceda2ae7930
Juan-Pablo Velez <jpvelez@gmail.com>
2016-03-02 14:42:54 -0500

    Output jupyter notebook from Makefile; pickle clean data; refactor cleaning script to use higher-order functions

--- Makefile
+++ Makefile
@@ -1,23 +1,38 @@
+############
+# CLEANING #
+############
 # Make directory for clean dataset, if it doesn't already exist.
 clean_data:
 	mkdir -p $@
 
 # Clean products, product classes, promotions, and transactions.
-clean_data/product.csv: raw_data/product.csv clean_dataset.py clean_data
+clean_data/product.pkl: raw_data/product.csv clean_dataset.py clean_data
 	cat $< | python clean_dataset.py $@
 
-clean_data/product_class.csv: raw_data/product_class.csv clean_dataset.py clean_data
+clean_data/product_class.pkl: raw_data/product_class.csv clean_dataset.py clean_data
 	cat $< | python clean_dataset.py $@
 
-clean_data/promotion.csv: raw_data/promotion.csv clean_dataset.py clean_data
+clean_data/promotion.pkl: raw_data/promotion.csv clean_dataset.py clean_data
 	# Clean up media_type values. Use sed to replace ', ' with pipes after line 1.
 	# Note: make interprets '$' as a variable, '$$' makes sed filter work correctly.
 	cat $< | sed -e '2,$$ s/, / | /g' | python clean_dataset.py $@
 
-clean_data/transactions.csv: raw_data/transactions.csv clean_dataset.py clean_data
+clean_data/transactions.pkl: raw_data/transactions.csv clean_dataset.py clean_data
 	# Clean up fact_count values. Used sed to strip ');' characters from integers.
 	cat $< | sed 's/1);/1/g' | python clean_dataset.py $@
 
 # Clean all datasets, if they haven't been cleaned already.
 .PHONY: clean_datasets
-clean_datasets: clean_data/product.csv clean_data/product_class.csv clean_data/promotion.csv clean_data/transactions.csv
+clean_datasets: clean_data/product.pkl clean_data/product_class.pkl clean_data/promotion.pkl clean_data/transactions.pkl
+
+
+############
+# ANALYSIS #
+############
+# Run sanity check analysis, which lives in a jupyter notebook, and save output to HTML.
+notebooks/sanity_check.html: clean_data/product.pkl clean_data/product_class.pkl clean_data/promotion.pkl clean_data/transactions.pkl notebooks/sanity_check.ipynb
+	# Assign first 4 input files, the dataset filenames, to DATASET var.
+	$(eval DATASETS='$(wordlist 1, 4, $^)')
+	# Execute notebook cells with datasets as input, and send HTML output to stdout.
+  # Dataset filenames are passed to notebook using temp env var `datasets`.
+	datasets=$(DATASETS) jupyter nbconvert --execute notebooks/sanity_check.ipynb --to html --stdout > $@
--- clean_dataset.py
+++ clean_dataset.py
@@ -10,6 +10,8 @@ cat product.csv | ./clean_dataset.py clean_product.csv
 '''
 
 import sys
+from functools import partial
+from operator import methodcaller
 import numpy as np
 import pandas as pd
 
@@ -24,17 +26,35 @@ def clean_string_col(series):
         return series       # Non-string series.
 
 
-def string_to_float(series):
+# Functions that change dtype of ndarrays/pandas series.
+converter_functions = {'datetime': pd.to_datetime,
+                       # Treat ndarray type casting method as function
+                       # with predefined positional argument.
+                       'float': methodcaller('astype', 'float64')}
+
+
+def convert_str_to(dtype, series):
     '''
-    Convert numerical string series to float.
+    Convert string series with "wrong" dtype to specified dtype.
+
+    If dtype is float, numerical string series convert to float.
+    If dtype is datetime, string series with dates convert to datetime.
+    Other series are not touched.
     '''
-    if series.dtype == np.dtype('O'):  # Don't cast int columns to float.
+    if series.dtype == np.dtype('O'):  # Don't re-cast numeric series.
         try:
-            return series.astype(np.dtype('float64'))
-        except ValueError:
-            return series              # Non-numerical string series.
+            return converter_functions[dtype](series)
+        except ValueError:             # String series that can't be re-cast.
+            return series
     else:
         return series
+
+
+# Manufacture string conversion functions using functools.partial,
+# a higher-order function that takes in an abstract function
+# - convert_str_to, in this case - and returns a parameterized copy.
+str_to_float = partial(convert_str_to, 'float')
+str_to_date = partial(convert_str_to, 'datetime')
 
 if __name__ == '__main__':
     # IO: Spool stdin data stream into in-memory DataFrame.
@@ -45,6 +65,10 @@ if __name__ == '__main__':
     dataset.columns = [col_name.strip() for col_name in dataset.columns]
 
     # COLUMN VALUES: Remove apostrophes from string column values,
-    # cast numerical string columns to floats, save to disk.
+    # cast numerical string columns to floats, cast string columns
+    # with dates to datetime, pickle dataset to preserve data types.
     out_filename = sys.argv[1]
-    dataset.apply(clean_string_col).apply(string_to_float).to_csv(out_filename)
+    dataset.apply(clean_string_col) \
+           .apply(str_to_float)     \
+           .apply(str_to_date)      \
+           .to_pickle(out_filename)
